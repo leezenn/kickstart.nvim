34116082f966ce5e87169e39278e8e3b178e343a
leezenn <serhii.lyzen@gmail.com>
2024-04-25 02:38:45 +0300

    someplugs

---
+++ lua/custom/plugins/folke.lua
@@ -0,0 +1,29 @@
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+  { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+  { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+  { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+  { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+  { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+},
+
+
+{
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- NOTE: https://github.com/folke/todo-comments.nvim#%EF%B8%8F-configuration
+  },
+}
+
+}
+
+  -- NOTE: Try this: https://github.com/akinsho/toggleterm.nvim
+
